c67036132b4372c63a45585314190d9d1e007257
Bphreaker <63411948+Bphreaker@users.noreply.github.com>
2021-02-11 18:18:52 +0000

    Update ASWebAuthSession.m

--- src/ios/ASWebAuthSession.m
+++ src/ios/ASWebAuthSession.m
@@ -23,7 +23,7 @@ ASWebAuthenticationSession *_asAuthenticationVC;
                                        if (callbackURL) {
                                            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: callbackURL.absoluteString];
                                        } else {
-                                           if([error length] == 0){
+                                           if(error == nil){
                                                 result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"cancel"];
                                            } else {
                                                 result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"error"];
